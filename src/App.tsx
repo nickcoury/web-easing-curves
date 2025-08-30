c8835322ae6ad9812a181a4f1c34bbd667e862f4
Nick Coury <nickcoury@gmail.com>
2025-08-29 19:55:20 -0700

    Initial project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>CSS Easing Curves</h1>
+      </header>
+      <main>
+        {/* Empty for now */}
+      </main>
+    </div>
+  )
+}
+
+export default App
